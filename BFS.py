89532af900937d095019acd29f6f7903fab0e990
marvingfx <marvinstraathof@gmail.com>
2015-11-06 20:35:12 +0100

    Rename

--- BFS.py
+++ BFS.py
@@ -6,13 +6,13 @@ from timeit import default_timer as timer
 # check if file is supplied
 if len(sys.argv) <= 1:
     print "No file is supplied"
-    print "Usage: python DFS.py <board.txt>"
+    print "Usage: python BFS.py <board.txt>"
     sys.exit()
 
 # check if file exists
 elif not os.path.isfile(sys.argv[1]):
     print "File can't be loaded"
-    print "Usage: python DFS.py <board.txt>"
+    print "Usage: python BFS.py <board.txt>"
     sys.exit()
 
 # load the board
@@ -30,4 +30,4 @@ print end - start
 # check for move validity
 # move
 # archive boards and steps
-# class board + hasing
+# class board + hashing
