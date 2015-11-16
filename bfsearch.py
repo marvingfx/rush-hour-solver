44d3fba4712bf9ec8ee45da142fd9730864e2b7d
marvingfx <marvinstraathof@gmail.com>
2015-11-16 14:10:03 +0100

    Further enhancements

--- bfsearch.py
+++ bfsearch.py
@@ -2,6 +2,18 @@ import sys, rushutils, os.path, collections
 from timeit import default_timer as timer
 
 
+def bfs():
+    while len(queue) > 0:
+        node = queue.pop(0)
+        for move in node.get_moves():
+            child = node.move(move[0], move[1])
+            if move[0] == '?':
+                if child.win():
+                    return child
+            if child.get_hash() not in states:
+                states.add(child.get_hash())
+                queue.append(child)
+
 # check if file is supplied
 if len(sys.argv) <= 1:
     print "No file is supplied"
@@ -16,33 +28,33 @@ elif not os.path.isfile(sys.argv[1]):
 
 # load board from file
 else:
+
+    # initialize root node
     root = rushutils.Board(None, None, None, None)
     root.load_from_file(sys.argv[1])
+
+    # initialize queue and states archive
     states = set()
     queue = list()
     states.add(root.get_hash())
     queue.append(root)
 
-
-def bfs():
-    while len(queue) > 0:
-        node = queue.pop(0)
-        for move in node.get_moves():
-            child = node.move(move[0], move[1])
-            if child.win():
-                return child
-            if child.get_hash() not in states:
-                states.add(child.get_hash())
-                queue.append(child)
-
-
+# start the timer
 start = timer()
+
+# get first route to solution
 current = bfs()
-print len(states)
+
+# stop the timer
 end = timer()
+
+# get the moves from to the winning state
 moves = collections.deque()
 while current.parent is not None:
     moves.appendleft(current.moved)
     current = current.parent
+
+# print results
+print "\nSolved in %d moves, in the time of %f seconds" % (len(moves), (end - start))
 print moves
-print end - start
+print
--- rushutils.py
+++ rushutils.py
@@ -1,101 +1,130 @@
-import csv, collections
+import csv, collections, math
 from bitarray import bitarray
 
 
 class Board:
     width = 6
+    winning_tile = 36
 
-    def __init__(self, parent, board, cars, moved):
+    def __init__(self, parent, board, vehicles, moved):
         self.parent = parent
         self.board = board
-        self.cars = cars
+        self.vehicles = vehicles
         self.moved = moved
 
+    # TODO: fix issue for board3
     def get_hash(self):
         return self.board.tobytes()
 
     def load_from_file(self, path):
+        """
+        loads board and vehicles from a csv file
+        :param path: path to file
+        """
+
         # construct board
         string_from_board = ""
         self.board = bitarray()
+
+        # read file
         with open(path) as f:
             for row in csv.reader(f):
-                self.width = len(row)
+                Board.width = len(row)
                 for value in row:
                     string_from_board += value
 
-                    # bit array representation of board
+                    # update bit array representation of board
                     if value == '.':
                         self.board.append(False)
                     else:
                         self.board.append(True)
 
-        # get the cars
-        self.cars = dict()
+        Board.winning_tile = int(math.floor((len(string_from_board) / 2) - 1))
+
+        # get the vehicles
+        self.vehicles = dict()
         values = collections.Counter(string_from_board)
         for name in values:
             first = string_from_board.index(name)
             last = string_from_board.rindex(name)
             if not name == '.':
+
+                # add vehicle to dictionary
                 if last - first > 2:
-                    self.cars[name] = ('v', first, last)
+                    self.vehicles[name] = ('v', first, last)
                 else:
-                    self.cars[name] = ('h', first, last)
+                    self.vehicles[name] = ('h', first, last)
 
     def get_moves(self):
+        """
+        checks which vehicles can move
+        :return: array with possible moves
+        """
         moves = []
-        for car in self.cars:
-            if self.cars[car][0] == 'h':
+        for vehicle in self.vehicles:
+            if self.vehicles[vehicle][0] == 'h':
 
-                # check if car can go forwards
-                if not self.cars[car][1] % self.width == 0 and self.board[self.cars[car][1] - 1] == 0:
-                        moves.append([car, -1])
+                # check if vehicle can go forwards
+                if not self.vehicles[vehicle][1] % self.width == 0 and self.board[self.vehicles[vehicle][1] - 1] == 0:
+                        moves.append([vehicle, -1])
 
-                # check if car can go backwards
-                if not (self.cars[car][2] - self.width + 1) % self.width == 0 and self.board[self.cars[car][2] + 1] == 0:
-                        moves.append([car, 1])
+                # check if vehicle can go backwards
+                if not (self.vehicles[vehicle][2] - self.width + 1) % self.width == 0 and self.board[self.vehicles[vehicle][2] + 1] == 0:
+                        moves.append([vehicle, 1])
             else:
 
-                # check if car can go upwards
-                if self.cars[car][1] >= self.width and self.board[self.cars[car][1] - self.width] == 0:
-                        moves.append([car, -1])
+                # check if vehicle can go upwards
+                if self.vehicles[vehicle][1] >= self.width and self.board[self.vehicles[vehicle][1] - self.width] == 0:
+                        moves.append([vehicle, -1])
 
-                # check if car can go downwards
-                if self.cars[car][2] < self.width * self.width - self.width and self.board[self.cars[car][2] + self.width] == 0:
-                        moves.append([car, 1])
+                # check if vehicle can go downwards
+                if self.vehicles[vehicle][2] < self.width * self.width - self.width and self.board[self.vehicles[vehicle][2] + self.width] == 0:
+                        moves.append([vehicle, 1])
 
         return moves
 
-    def move(self, car, move):
-        node = Board(self, self.board[:], self.cars.copy(), (car, move))
+    def move(self, vehicle, move):
+        """
+        moves the vehicle on the board
+        :param vehicle: id of vehicle that can be moved
+        :param move: direction in which the car can move
+        :return: new Board instance
+        """
 
-        old = node.cars.get(car)
+        # create new node
+        node = Board(self, self.board[:], self.vehicles.copy(), (vehicle, move))
+
+        # initialize variables
+        first = node.vehicles[vehicle][1]
+        last = node.vehicles[vehicle][2]
         update = []
 
-        if node.cars[car][0] == 'h':
+        # move horizontally orientated cars
+        if node.vehicles[vehicle][0] == 'h':
             if move > 0:
-                node.board[node.cars[car][1]] = 0
-                node.board[node.cars[car][2] + 1] = 1
-                update.append((old[1] + 1, old[2] + 1))
+                node.board[first] = 0
+                node.board[last + 1] = 1
+                update.append((first + 1, last + 1))
             else:
-                node.board[node.cars[car][1] - 1] = 1
-                node.board[node.cars[car][2]] = 0
-                update.append((old[1] - 1, old[2] - 1))
+                node.board[first - 1] = 1
+                node.board[last] = 0
+                update.append((first - 1, last - 1))
+
+        # move vertically orientated cars
         else:
             if move > 0:
-                node.board[node.cars[car][1]] = 0
-                node.board[node.cars[car][2] + self.width] = 1
-                update.append((old[1] + self.width, old[2] + self.width))
+                node.board[first] = 0
+                node.board[last + self.width] = 1
+                update.append((first + self.width, last + self.width))
             else:
-                node.board[node.cars[car][1] - self.width] = 1
-                node.board[node.cars[car][2]] = 0
-                update.append((old[1] - self.width, old[2] - self.width))
+                node.board[first - self.width] = 1
+                node.board[last] = 0
+                update.append((first - self.width, last - self.width))
 
-        # update car
-        node.cars.pop(car)
-        node.cars[car] = (old[0], update[0][0], update[0][1])
+        # update vehicle dictionary
+        node.vehicles[vehicle] = (node.vehicles[vehicle][0], update[0][0], update[0][1])
+
         return node
 
     def win(self):
-        # TODO: remove hardcode
-        return self.cars['?'][2] == 17
+        return self.vehicles['?'][2] == Board.winning_tile
