2c12c218d8c03bd6a7ed7f30ce8a5991c932baf1
marvingfx <marvinstraathof@gmail.com>
2015-11-19 10:57:06 +0100

    Renamed id.py to dfsearch.py

---
+++ dfsearch.py
@@ -0,0 +1,61 @@
+import sys, rushutils, os.path
+from timeit import default_timer as timer
+
+
+def dfs():
+    while len(stack) > 0:
+        node = stack.pop()
+        for move in node.get_moves():
+            child = node.move(move[0], move[1])
+            if move[0] == 0:
+                if child.win():
+                    return child
+            if child.get_hash() not in states:
+                states.add(child.get_hash())
+                stack.append(child)
+
+# check if file is supplied
+if len(sys.argv) <= 1:
+    print "No file is supplied"
+    print "Usage: python bfsearch.py <board.txt>"
+    sys.exit()
+
+# check if file exists
+elif not os.path.isfile(sys.argv[1]):
+    print "File can't be loaded"
+    print "Usage: python bfsearch.py <board.txt>"
+    sys.exit()
+
+# load board from file
+else:
+
+    # initialize root node
+    root = rushutils.Board(None, None, None, None)
+    root.load_from_file(sys.argv[1])
+
+    # initialize queue and states archive
+    states = set()
+    stack = list()
+    states.add(root.get_hash())
+    stack.append(root)
+
+# start the timer
+start = timer()
+
+# get first route to solution
+current = dfs()
+
+# stop the timer
+end = timer()
+
+# get the moves from to the winning state
+moves = []
+while current.parent is not None:
+    moves.append(current.moved)
+    current = current.parent
+moves.reverse()
+
+# print results
+print "\nSolved in %d moves, in the time of %f seconds" % (len(moves), (end - start))
+print moves
+print
