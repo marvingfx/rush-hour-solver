bb42994a5d54f4cd1f6607e9d73200d82a0e5829
marvingfx <marvinstraathof@gmail.com>
2015-12-02 15:01:51 +0100

    Astar now deletes duplicate instances

--- astar.py
+++ astar.py
@@ -13,6 +13,8 @@ def astar():
             if child.get_hash() not in states:
                 states.add(child.get_hash())
                 heapq.heappush(pqueue, child)
+            else:
+                del child
 
 # check if file is supplied
 if len(sys.argv) <= 1:
