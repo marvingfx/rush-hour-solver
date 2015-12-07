e665ec0bfedd6129bf015b947d97b4948ba55525
marvingfx <marvinstraathof@gmail.com>
2015-12-07 10:30:29 +0100

    visualisation is now suitable for implementation

--- Visualisation.py
+++ Visualisation.py
@@ -1,115 +1,126 @@
-import pygame, math, random, sys
+import pygame, random, sys
 from pygame.locals import *
 from collections import Counter
 
-# hardcode, to be removed
-WIDTH = 480
-TILE = 80
-board = list("..abbc..a..c..a??c...eddhgge..h..eff")
-bwidth = int(math.sqrt(len(board)))
-vehicles = [('?', True, 15, 16), ('a', False, 2, 14), ('c', False, 5, 17), ('b', True, 3, 4), ('e', False, 21, 33), ('d', True, 22, 23), ('g', True, 25, 26), ('f', True, 34, 35), ('h', False, 24, 30)]
-solution = [(1, 1), (3, -1), (3, -1), (8, -1), (8, -1), (6, -1), (1, 1), (1, 1), (0, -1), (0, -1), (4, -1), (4, -1), (4, -1), (5, -1), (2, 1), (2, 1), (7, -1), (2, 1), (8, -1), (8, -1), (0, -1), (1, -1), (1, -1), (6, 1), (6, 1), (6, 1), (1, 1), (1, 1), (0, 1), (8, 1), (3, -1), (8, 1), (8, 1), (0, -1), (1, -1), (1, -1), (1, -1), (5, -1), (5, -1), (4, 1), (7, -1), (7, -1), (8, 1), (5, -1), (1, 1), (3, 1), (3, 1), (3, 1), (1, -1), (3, 1), (4, -1), (5, 1), (5, 1), (5, 1), (1, 1), (1, 1), (8, -1), (7, -1), (1, 1), (0, 1), (8, -1), (8, -1), (8, -1), (0, -1), (1, -1), (1, -1), (1, -1), (5, -1), (5, -1), (4, 1), (5, -1), (1, 1), (6, -1), (6, -1), (4, 1), (4, 1), (6, -1), (1, 1), (1, 1), (0, 1), (0, 1), (0, 1), (0, 1)]
-colorlist = Counter(board)
 
-# assigns a random color to each vehicle
-for vehicle in colorlist:
-    if not vehicle == '?':
-        colorlist[vehicle] = (int(random.random() * 256), int(random.random() * 256), int(random.random() * 256))
+class Visualisation:
+    def __init__(self, root, solution, tile=80):
+        self.vehicles = root.vehicles
+        self.solution = solution
+        self.tile = tile
+        self.width = (root.width + 1) * self.tile
+        self.colors = self.get_colors(root.board)
 
-# assign red color to main vehicle
-colorlist['?'] = (255, 0, 0)
+        # initialize pygame and window
+        pygame.init()
+        self.window = pygame.display.set_mode((self.width, self.width))
+        pygame.display.set_caption('Rush Hour')
+        self.window.fill((255, 255, 255))
+        self.draw_board()
+        pygame.display.update()
 
-def update_board(move):
-    """
-    updates the list representation of the board
-    :param move: tuple containing the index of the vehicle and the movement
-    """
-    vehicle = vehicles[move[0]]
-    if vehicle[1]:
-        # move right
-        if move[1] > 0:
-            board[vehicle[3] + 1] = vehicle[0]
-            board[vehicle[2]] = '.'
-        # move left
-        else:
-            board[vehicle[2] - 1] = vehicle[0]
-            board[vehicle[3]] = '.'
-        vehicles[move[0]] = (vehicle[0], vehicle[1], vehicle[2] + move[1], vehicle[3] + move[1])
+        # pause visualisation (wait for input)
+        self.pause()
 
-    else:
-        # move down
-        if move[1] > 0:
-            board[vehicle[3] + bwidth] = vehicle[0]
-            board[vehicle[2]] = '.'
-        # move up
-        else:
-            board[vehicle[2] - bwidth] = vehicle[0]
-            board[vehicle[3]] = '.'
-        vehicles[move[0]] = (vehicle[0], vehicle[1], vehicle[2] + (move[1] * bwidth), vehicle[3] + (move[1] * bwidth))
+        # start visualisation
+        self.run()
 
-def draw_board():
-    """
-    draws the current board on the window
-    """
-    for index, tile in enumerate(board):
-        if not tile == '.':
-             pygame.draw.rect(window, colorlist[tile], ((index % bwidth) * TILE, (index / bwidth) * TILE, TILE, TILE))
+    def check_input(self):
+        """
+        checks for input which pauses or quits the visualisation
+        """
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                self.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    self.exit()
+                self.pause()
 
-def exit():
-    """
-    quits the current program
-    """
-    pygame.quit()
-    sys.exit()
+    @staticmethod
+    def get_colors(board):
+        """
+        creates random colors for each vehicle (except for the main vehicle)
+        :param board: board representation from root node
+        :return: dictionary containing colors
+        """
 
-def pause():
-    """
-    pauses the game, waits for input from user
-    """
-    while True:
-        event = pygame.event.wait()
-        if event.type == QUIT:
-            exit()
-        if event.type == KEYDOWN:
-            break
+        array = list()
+        for row in board:
+            array.extend(row)
 
-def check_pause():
-    """
-    checks if a button is pressed
-    """
+        # create dictionary with a color for each index found in the board
+        indexes = Counter(array)
+        indexes.pop(None)
+        for index in indexes:
+            indexes[index] = (int(random.random() * 256), int(random.random() * 256), int(random.random() * 256))
 
-    # quit program
-    for event in pygame.event.get(QUIT):
-        exit()
+        # recolor the main vehicle
+        indexes[0] = (255, 0, 0)
 
-    # pause program
-    for event in pygame.event.get(KEYDOWN):
-        pause()
+        return indexes
 
-# actual visualisation
-pygame.init()
-window = pygame.display.set_mode((WIDTH, WIDTH))
-pygame.display.set_caption('Rush Hour')
-window.fill((255, 255, 255))
-draw_board()
-pygame.display.update()
+    @staticmethod
+    def exit():
+        """
+        quit the visualisation
+        """
+        pygame.quit()
+        sys.exit()
 
-# pause the game so that it doesn't start automatically
-pause()
+    def pause(self):
+        """
+        pause the visualisation, wait for input
+        """
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    self.exit()
+                elif event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        self.exit()
+                    return False
 
-# iterate over the moves in the solution
-while len(solution) > 0:
-    check_pause()
-    window.fill((255, 255, 255))
-    update_board(solution.pop(0))
-    draw_board()
-    pygame.display.update()
-    pygame.time.wait(150)
+    def run(self):
+        """
+        start the visualisation, which will pause after completing
+        """
 
-# wait for input to quit the game
-while True:
-    event = pygame.event.wait()
-    if event.type == QUIT:
-        exit()
-    if event.type == KEYDOWN:
-        exit()
+        # iterate over the moves in the solution
+        while len(self.solution) > 0:
+            self.check_input()
+            self.window.fill((255, 255, 255))
+            self.update_vehicles(self.solution.pop(0))
+            self.draw_board()
+            pygame.display.update()
+            pygame.time.wait(150)
+
+        # wait for input to quit
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT or event.type == KEYDOWN:
+                    self.exit()
+
+    def update_vehicles(self, step):
+        index = step[0]
+        move = step[1]
+
+        # move the vehicle
+        vehicle = self.vehicles[index]
+        self.vehicles[index] = (vehicle[0], vehicle[1], vehicle[2] + move, vehicle[3] + move)
+
+    def draw_board(self):
+        """
+        draw the current vehicles on the window
+        """
+        for index, vehicle in enumerate(self.vehicles):
+            length = (vehicle[3] - vehicle[2]) + 1
+
+            # draw horizontally orientated vehicles
+            if vehicle[0]:
+                pygame.draw.rect(self.window, self.colors[index], (vehicle[2] * self.tile, vehicle[1] * self.tile,
+                                                                   length * self.tile, self.tile))
+            # draw vertically orientated vehicles
+            else:
+                pygame.draw.rect(self.window, self.colors[index], (vehicle[1] * self.tile, vehicle[2] * self.tile,
+                                                                   self.tile, length * self.tile))
