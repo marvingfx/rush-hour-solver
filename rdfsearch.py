97e063c84700f26a7d26737ee1a88a798fb62445
marvingfx <marvinstraathof@gmail.com>
2015-12-18 10:13:44 +0100

    Implemented stochastic depth first search

---
+++ rdfsearch.py
@@ -0,0 +1,86 @@
+import sys, rushutils, os.path, random
+from timeit import default_timer as timer
+
+
+def rdfs():
+    """
+    calculates a path to a winning state
+    """
+
+    # initialize stack and closed archive
+    closed = dict()
+    closed[root.get_hash()] = None
+    stack = list()
+    stack.append(root)
+
+    # set depth limit
+    maximum = 100000
+
+    # start the timer
+    start = timer()
+
+    while len(stack):
+
+        # get the top node from the stack
+        node = stack.pop()
+
+        if node.depth < maximum - 1:
+            # generate all possible children
+            moves = node.get_moves()
+            random.shuffle(moves)
+            for move in moves:
+                child = node.move(move[0], move[1])
+
+                # check if child has already been processed
+                if child.get_hash() not in closed:
+
+                    # add child to closed list and to the stack
+                    closed[child.get_hash()] = [node.vehicles, move]
+                    stack.append(child)
+
+                # check if current child is a solution
+                if move[0] == 0:
+                    if child.win():
+
+                        # print results
+                        print "\nExplored %d states in %f seconds" % (len(closed), (timer() - start))
+                        print "Current shortest path takes %d steps" % child.depth
+
+                        # start over with new maximum
+                        stack = list()
+                        stack.append(root)
+                        closed.clear()
+                        closed[root.get_hash()] = None
+                        start = timer()
+                        maximum = child.depth
+                        break
+
+        # start over
+        else:
+            stack = list()
+            stack.append(root)
+            closed.clear()
+            closed[root.get_hash()] = None
+            start = timer()
+
+# check if file is supplied
+if len(sys.argv) <= 1:
+    print "No file is supplied"
+    print "Usage: python rdfsearch.py <board.txt>"
+    sys.exit()
+
+# check if file exists
+elif not os.path.isfile(sys.argv[1]):
+    print "File can't be loaded"
+    print "Usage: python rdfsearch.py <board.txt>"
+    sys.exit()
+
+# load board from file
+else:
+
+    # initialize root node
+    root = rushutils.Board()
+    root.load_from_file(sys.argv[1])
+
+# get a path
+rdfs()
