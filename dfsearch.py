6bc55134a211ecd15772beaf78b5546e6d03b76f
marvingfx <marvinstraathof@gmail.com>
2015-12-18 09:34:52 +0100

    Added comments

--- dfsearch.py
+++ dfsearch.py
@@ -1,18 +1,34 @@
-import sys, rushutils_uninformed, os.path, visualisation
+import sys, rushutils, os.path, visualisation
 from timeit import default_timer as timer
 
 
 def dfs():
-    while len(stack) > 0:
+    """
+    calculates a path to a winning state
+    """
+    while len(stack):
+
+        # get the top node from the stack
         node = stack.pop()
-        for move in node.get_moves():
+
+        # generate all possible children
+        moves = node.get_moves()
+        for move in moves:
             child = node.move(move[0], move[1])
-            if move[0] == 0:
-                if child.win():
-                    solutions.append(child)
-            if child.get_hash() not in states:
-                states.add(child.get_hash())
+
+            # check if child has already been processed
+            if child.get_hash() not in closed:
+
+                # add child to closed list and to the stack
+                closed[child.get_hash()] = [node.vehicles, move]
                 stack.append(child)
+
+            # check if current child is a solution
+            if move[0] == 0:
+
+                # add winning configuration to solutions list
+                if child.win():
+                    solutions.append(child.get_hash())
 
 # check if file is supplied
 if len(sys.argv) <= 1:
@@ -30,15 +46,15 @@ elif not os.path.isfile(sys.argv[1]):
 else:
 
     # initialize root node
-    root = rushutils_uninformed.Board()
+    root = rushutils.Board()
     root.load_from_file(sys.argv[1])
 
-    # initialize queue and states archive
-    states = set()
+    # initialize queue, solutions list, and closed archive
+    closed = dict()
+    closed[root.get_hash()] = None
     stack = list()
-    solutions = list()
-    states.add(root.get_hash())
     stack.append(root)
+    solutions = list()
 
 # start the timer
 start = timer()
@@ -49,24 +65,25 @@ dfs()
 # stop the timer
 end = timer()
 
-# get the moves from to the winning state
+# get the shortest path to solution from all possible solutions
 shortest_solution = list()
-for path in solutions:
+for node in solutions:
     solution = list()
-    current = path
-    while current.parent is not None:
-        solution.append(current.moved)
-        current = current.parent
+    while closed[node] is not None:
+        solution.append(closed[node][1])
+        node = closed[node][0]
+
+    # determine if solution is shorter than current shortest solution
     if len(shortest_solution) == 0 or len(shortest_solution) > len(solution):
         solution.reverse()
         shortest_solution = solution
 
 # print results
-print "\nExplored %d states in %f seconds" % (len(states), (end - start))
+print "\nExplored %d states in %f seconds" % (len(closed), (end - start))
 print "\nFound %d solutions, shortest solution takes %d moves" % (len(solutions), len(shortest_solution))
 print shortest_solution
 print
 
 # start visualisation if wanted
-if raw_input("visualisation? (Y/N): ").lower() == 'y':
+if raw_input("View visualisation of solution? (Y/N): ").lower() == 'y':
     vis = visualisation.Visualisation(root, shortest_solution)
